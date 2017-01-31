1b837b40cff3fedadd13cf91352a1e576fbfc5bb
Johan <j0han_lindell@hotmail.com>
2017-01-31 16:26:29 +0100

    FIXADE DET JÄVLA FELET

---
+++ DataAccess/Models/Post.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccess.Models
+{
+    public class Post
+    {
+        public virtual int Id { get; set; }
+        public virtual User Author { get; set; }
+        public virtual Category Category { get; set; }
+        public virtual string Title { get; set; }
+        public virtual string Content { get; set; }
+        public virtual string CreatedAt { get; set; }
+        public virtual bool Formal { get; set; }
+        public virtual ISet<Comment> Comments { get; set; }
+        public virtual ISet<File> Files { get; set; }
+        public Post()
+        {
+            Comments = new HashSet<Comment>();
+            Files = new HashSet<File>();
+        }
+    }
+
+
+}
