7ceb09279bd72c4e7d97aa9951aedb35f6c04304
Noa <Noa@NOA-PC>
2017-01-31 16:35:39 +0100

    Fixed everything

--- DataAccess/Models/Comment.cs
+++ DataAccess/Models/Comment.cs
@@ -6,10 +6,12 @@ using System.Threading.Tasks;
 
 namespace DataAccess.Models
 {
-    public class File
+   public class Comment
     {
         public virtual int Id { get; set; }
+        public virtual User Author { get; set; }
         public virtual Post Post { get; set; }
-        public virtual string FilePath { get; set; }
+        public virtual string Content { get; set; }
+        public virtual DateTime CreatedAt { get; set; }
     }
 }
