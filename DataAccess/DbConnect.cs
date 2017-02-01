cdbe693916c9b51b172da9850dd310d3e34b424f
Noa <Noa@NOA-PC>
2017-02-01 12:43:00 +0100

    Added repository. Added sessionfactory with connectionstring.

---
+++ DataAccess/DbConnect.cs
@@ -0,0 +1,34 @@
+﻿using DataAccess.Mappings;
+using FluentNHibernate.Cfg;
+using FluentNHibernate.Cfg.Db;
+using NHibernate;
+
+namespace DataAccess
+{
+    // This class is responsible for the connection to the localDB database.
+    public class DbConnect
+    {
+        private static ISessionFactory _sessionFactory;
+        public static ISessionFactory SessionFactory
+        {
+            get
+            {
+                if (_sessionFactory == null)
+                {
+                    _sessionFactory = BuildSessionFactory();
+                }
+                return _sessionFactory;
+            }
+        }
+
+        private static ISessionFactory BuildSessionFactory()
+        {
+            return Fluently.Configure()
+                .Database(MsSqlConfiguration.MsSql2012
+                .ConnectionString("data source=(LocalDB)\\MSSQLLocalDB;attachdbfilename=|DataDirectory|\\InfoCom.mdf;integrated security=True"))
+                .Mappings(m => m.FluentMappings.AddFromAssemblyOf<UserMap>())
+                .BuildSessionFactory();
+        }
+    }
+
+}
---
+++ DataAccess/Repositories/UserRepository.cs
@@ -0,0 +1,78 @@
+﻿using DataAccess.Models;
+using NHibernate;
+using NHibernate.Linq;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccess.Repositories
+{
+    public static class UserRepository
+    {
+        public static User get(int id)
+        {
+            try
+            {
+                using (var session = DbConnect.SessionFactory.OpenSession())
+                {
+                    var user = session.Query<User>().FirstOrDefault(x => x.Id == id);
+                    return user;
+                }
+            }
+            catch (Exception ex)
+            {
+                Debug.WriteLine(ex.Message);
+                
+            }
+            return null;
+        }
+
+        public static bool delete(int id)
+        {
+            var response = false;
+
+            try
+            {
+                using (var session = DbConnect.SessionFactory.OpenSession())
+                {
+                    var user = session.Query<User>().FirstOrDefault(x => x.Id == id);
+                    ITransaction transaction = session.BeginTransaction();
+                    session.Delete(user);
+                    transaction.Commit();
+                    response = true;
+                }
+            }
+            catch (Exception ex)
+            {
+                Debug.WriteLine(ex.Message);
+
+            }
+            return response;
+
+        }
+
+        public static bool add(User user)
+        {
+            var response = false;
+
+            try
+            {
+                using (var session = DbConnect.SessionFactory.OpenSession())
+                {
+                    session.Save(user);
+                    response = true;
+                }
+            }
+            catch (Exception ex)
+            {
+                Debug.WriteLine(ex.Message);
+
+            }
+            return response;
+        }
+    }
+}
+
