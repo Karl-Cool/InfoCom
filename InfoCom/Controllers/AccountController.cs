9b0e2c00a59bee5577658cf3c3930876347af744
Johan <j0han_lindell@hotmail.com>
2017-02-01 15:14:13 +0100

    AccountController, LoginViewModel

---
+++ InfoCom/Controllers/AccountController.cs
@@ -0,0 +1,79 @@
+﻿using DataAccess;
+using DataAccess.Models;
+using System;
+using System.Linq;
+using System.Security.Claims;
+using System.Web.Mvc;
+using NHibernate.Linq;
+using InfoCom.ViewModels;
+
+namespace InfoCom.Controllers
+{
+    public class AccountController : Controller
+    {
+        // GET: Account
+        [AllowAnonymous]
+        public ActionResult Login()
+        {
+
+            return View();
+        }
+        [AllowAnonymous]
+        [HttpPost]
+        public ActionResult Login(LoginViewModel model)
+        {
+            try
+            {
+                if (!ModelState.IsValid)
+                {
+                    return View(model);
+                }
+
+                using (var session = DbConnect.SessionFactory.OpenSession())
+                {
+                    var usernameCheck = session.Query<User>().FirstOrDefault(x => x.Username == model.Username);
+                    if (usernameCheck == null)
+                    {
+                        TempData["Message"] = "Invalid username or password";
+                        TempData["Type"] = "alert-danger";
+                        return View(model);
+                    }
+                    var passwordCheck =
+                        session.Query<User>()
+                            .Where(x => x.Username.Equals(model.Username))
+                            .Select(x => x.Password)
+                            .Single();
+
+                    if (BCrypt.Net.BCrypt.Verify(model.Password, passwordCheck))
+                    {
+                        var identity = new ClaimsIdentity(new[]
+                        {
+                            new Claim(ClaimTypes.NameIdentifier,
+                                session.Query<User>()
+                                    .Where(x => x.Username.Equals(model.Username))
+                                    .Select(x => x.Id)
+                                    .Single()
+                                    .ToString()),
+                            new Claim(ClaimTypes.Name, model.Username),
+                            new Claim(ClaimTypes.Authentication, "Admin")
+                        }, "InfoComCookie");
+
+                        var authManager = Request.GetOwinContext().Authentication;
+                        authManager.SignIn(identity);
+
+                        return RedirectToAction("Index", "Home");
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+                return RedirectToAction("Index", "Error");
+            }
+
+            TempData["Message"] = "Invalid username or password";
+            TempData["Type"] = "alert-danger";
+            return View(model);
+        }
+    }
+}
