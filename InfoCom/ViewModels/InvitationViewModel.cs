79475eb52cb3257cf33776a3a965c2388e343ef6
Marti <Marti@DESKTOP-R0K86O4>
2017-02-02 15:41:03 +0100

    Added show invitations functionality

---
+++ InfoCom/ViewModels/InvitationViewModel.cs
@@ -0,0 +1,18 @@
+﻿using DataAccess.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace InfoCom.ViewModels
+{
+    public class InvitationViewModel
+    {
+        public virtual List<Invitation> Invitations { get; set; }
+
+        public InvitationViewModel()
+        {
+            Invitations = new List<Invitation>();
+        }
+    }
+}
