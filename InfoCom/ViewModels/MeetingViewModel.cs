1cff7aded2b04240941eecb7e6b60bd38b29ff9c
Classon <Classon@DESKTOP-L3VK4BA>
2017-02-02 12:40:21 +0100

    Meeting functionality added

---
+++ InfoCom/ViewModels/MeetingViewModel.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+using DataAccess.Models;
+
+namespace InfoCom.ViewModels
+{
+    public class MeetingViewModel
+    {
+        [Required(ErrorMessage = "Enter a title")]
+        public string Title { get; set; }
+        [Required(ErrorMessage = "Enter a description")]
+        public string Description { get; set; }
+        public User Creator { get; set; }
+
+    }
+}
