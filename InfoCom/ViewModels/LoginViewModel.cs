14da01fe2de85859731bcc67adea5297366015ca
Johan <j0han_lindell@hotmail.com>
2017-02-02 11:58:52 +0100

    Fixed password field for login.

--- InfoCom/ViewModels/LoginViewModel.cs
+++ InfoCom/ViewModels/LoginViewModel.cs
@@ -7,6 +7,7 @@ namespace InfoCom.ViewModels
         [Required(ErrorMessage = "Enter a username")]
         public string Username { get; set; }
         [Required(ErrorMessage = "Enter a password")]
+        [DataType(DataType.Password)]
         public string Password { get; set; }
 
     }
