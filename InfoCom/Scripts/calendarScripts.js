ce203c86b30f638f7ab9661167e22273eb79619d
Jawolsson <antonolsson69@gmail.com>
2017-02-07 14:51:17 +0100

    Calendar now showing meetings from database

--- InfoCom/Scripts/calendarScripts.js
+++ InfoCom/Scripts/calendarScripts.js
@@ -1,39 +1,50 @@
 ﻿$(document).ready(function () {
-    var events = [
-    { Title: "Five K for charity", Date: new Date("02/13/2017") },
-    { Title: "Dinner", Date: new Date("02/25/2017") },
-    { Title: "Meeting with manager", Date: new Date("03/01/2017") }
-    ];
+    var events = [];
+    getMeetings();
 
-    $("#calendar").datepicker({
-        beforeShowDay: function (date) {
-            var result = [true, '', null];
-            var matching = $.grep(events, function (event) {
-                return event.Date.valueOf() === date.valueOf();
+    function getMeetings() {
+        $.getJSON('/api/calendar/get')
+            .done(function (data) {
+                $.each(data, function (i, item) {
+                    var dateobj = new Date(item.ConfirmedTime);
+                    var obj = {
+                        Id: item.Id, Title: item.Title, Date: new Date((dateobj.getMonth() + 1)
+                        + "/" + dateobj.getUTCDate() + "/" + dateobj.getFullYear())
+                    }
+                    events.push(obj);
+                });
+                $("#calendar").datepicker({
+                    beforeShowDay: function (date) {
+                        var result = [true, '', null];
+                        var matching = $.grep(events, function (event) {
+                            return event.Date.valueOf() === date.valueOf();
+                        });
+
+                        if (matching.length) {
+                            result = [true, 'highlight', null];
+                        }
+                        return result;
+                    },
+                    onSelect: function (dateText) {
+                        var date,
+                            selectedDate = new Date(dateText),
+                            i = 0,
+                            event = null;
+
+                        while (i < events.length && !event) {
+                            date = events[i].Date;
+
+                            if (selectedDate.valueOf() === date.valueOf()) {
+                                event = events[i];
+                            }
+                            i++;
+                        }
+                        if (event) {
+                            alert(event.Id + " " + event.Title + " " + event.Date);
+                        }
+                    }
+                });
             });
-
-            if (matching.length) {
-                result = [true, 'highlight', null];
-            }
-            return result;
-        },
-        onSelect: function (dateText) {
-            var date,
-                selectedDate = new Date(dateText),
-                i = 0,
-                event = null;
-
-            while (i < events.length && !event) {
-                date = events[i].Date;
-
-                if (selectedDate.valueOf() === date.valueOf()) {
-                    event = events[i];
-                }
-                i++;
-            }
-            if (event) {
-                alert(event.Title);
-            }
-        }
-    });
+    }
 });
+
