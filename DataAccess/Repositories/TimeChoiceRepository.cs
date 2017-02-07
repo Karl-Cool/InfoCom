c4fabc8d604ea049f5caabade5be59af157b2e55
Karl <Karl@diskoteket.eu>
2017-02-07 14:32:54 +0100

    Fixat attend på valda tider.

---
+++ DataAccess/Repositories/TimeChoiceRepository.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using DataAccess.Models;
+using NHibernate.Linq;
+using System.Diagnostics;
+using NHibernate;
+
+namespace DataAccess.Repositories
+{
+    public static class TimeChoiceRepository
+    {
+        public static TimeChoice Get(int id)
+        {
+            try
+            {
+                using (var session = DbConnect.SessionFactory.OpenSession())
+                {
+                    var timeChoice = session.Query<TimeChoice>().FirstOrDefault(x => x.Id == id);
+                    return timeChoice;
+                }
+            }
+            catch (Exception ex)
+            {
+                Debug.WriteLine(ex.Message);
+
+            }
+            return null;
+        }
+
+        public static List<TimeChoice> Get()
+        {
+            try
+            {
+                using (var session = DbConnect.SessionFactory.OpenSession())
+                {
+                    List<TimeChoice> timeChoiceList = session.Query<TimeChoice>().ToList();
+                    return timeChoiceList;
+                }
+            }
+            catch (Exception ex)
+            {
+                Debug.WriteLine(ex.Message);
+
+            }
+            return null;
+        }
+
+        public static bool Delete(int id)
+        {
+            var response = false;
+
+            try
+            {
+                using (var session = DbConnect.SessionFactory.OpenSession())
+                {
+                    var time = session.Query<TimeChoice>().FirstOrDefault(x => x.Id == id);
+                    ITransaction transaction = session.BeginTransaction();
+                    session.Delete(time);
+                    transaction.Commit();
+                    response = true;
+                }
+            }
+            catch (Exception ex)
+            {
+                Debug.WriteLine(ex.Message);
+
+            }
+            return response;
+
+        }
+
+        public static int Add(TimeChoice timeChoice)
+        {
+            int id = 0;
+
+            try
+            {
+                using (var session = DbConnect.SessionFactory.OpenSession())
+                {
+                    id = Convert.ToInt32(session.Save(timeChoice));
+                }
+            }
+            catch (Exception ex)
+            {
+                Debug.WriteLine(ex.Message);
+
+            }
+            return id;
+        }
+    }
+}
