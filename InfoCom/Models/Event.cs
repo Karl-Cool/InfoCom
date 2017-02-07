7f469c1f2234e0a3e0dc22f00f80be334672a95b
Johan Lindell <j0han_lindell@hotmail.com>
2017-02-07 15:07:20 +0100

    Fixed nullable datetime.

--- InfoCom/Models/Event.cs
+++ InfoCom/Models/Event.cs
@@ -6,7 +6,7 @@ namespace InfoCom.Models
     {
         public int Id { get; set; }
         public string Title { get; set; }
-        public DateTime ConfirmedTime { get; set; }
+        public DateTime? ConfirmedTime { get; set; }
 
     }
 }
