6fb21f1cd69ae32190fb100495348bec058db41c
Terenius <martin.terenius@live.se>
2017-02-08 13:27:14 +0100

    Add files via upload

---
+++ InfoCom/Scripts/deactivateUser.js
@@ -0,0 +1,18 @@
+﻿// Loads data into the popup window when delete user button is clicked
+function deactivateUserModal(id) {
+    var userId = id;
+    $('#confirm-deactivate-user-button').data("id", userId);
+}
+
+// Function that sends an ajax delete request to the User Api Controller to deactivate the selected user
+$('#confirm-deactivate-user-button').click(function () {
+    var id = $(this).data("id");
+    $.ajax({
+        type: "DELETE",
+        url: "/Api/Users/" + id,
+        contentType: "application/json"
+    })
+    .done(function (data) {
+        window.location.href = data.RedirectUrl;
+    });
+});
