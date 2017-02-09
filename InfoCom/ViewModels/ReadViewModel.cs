bb77ab3c4f72aeb56a0f81516c65eac84fb1b7d1
Johan Lindell <j0han_lindell@hotmail.com>
2017-02-09 20:50:50 +0100

    Comments

---
+++ InfoCom/ViewModels/ReadViewModel.cs
@@ -0,0 +1,12 @@
+﻿using DataAccess.Models;
+
+namespace InfoCom.ViewModels
+{
+    public class ReadViewModel
+    {
+        public int Id { get; set; }
+        public Post Post { get; set; }
+        public User Author { get; set; }
+        public string Content  { get; set; }
+    }
+}
