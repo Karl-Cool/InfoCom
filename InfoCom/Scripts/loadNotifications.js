6c6dc9f321083df67b5259335062743c5ded2ca5
Daniel Toll <daniel_toll@hotmail.com>
2017-02-10 10:50:01 +0100

    Meeting and Admin link in nav now drops down on hover

--- InfoCom/Scripts/loadNotifications.js
+++ InfoCom/Scripts/loadNotifications.js
@@ -26,3 +26,13 @@ function resetTimer() {
     clearInterval(time);
     time = setInterval(loadNotifications, intSec * 1000);
 }
+
+//Caret dropdown on hover
+
+$(document).ready(function () {
+    $('ul.nav li.dropdown').hover(function () {
+        $(this).find('.dropdown-menu').stop(true, true).delay(100).fadeIn(100);
+    }, function () {
+        $(this).find('.dropdown-menu').stop(true, true).delay(100).fadeOut(100);
+    });
+});
